66e79575e5882fe77009a812359a3202e6d123bc
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-06 19:09:29 +0100

    Feat: function wip

--- js/script.js
+++ js/script.js
@@ -8,8 +8,10 @@
 // va applicato uno sconto del 40% per gli over 65.
 
 const form = document.querySelector("form");
+//console.log(form);
 
 const nameInput = document.getElementById("name");
+console.log(nameInput);
 const kmInput = document.getElementById("km");
 const ageInput = document.getElementById("age");
 
@@ -19,20 +21,43 @@ const ticket = document.querySelector(".ticket");
 //const ticketBody = document.querySelector(".ticket-body");
 
 
-form.addEventListener("submit", function (event) {
+form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const name = nameInput.value;
-    const km = kmInput.value;
-    const age = ageInput.value;
+    const kmStr = kmInput.value;
+    const ageStr = ageInput.value;
+
+    const km = parseInt(kmStr);
+    const age = parseInt(ageStr);
+
+    let ticketRegPrice = parseFloat(Km * 0.21);
+    let ticket20 = parseFloat(ticketRegPrice - ((ticketRegPrice * 20) / 100));
+    let ticket40 = parseFloat(ticketRegPrice - ((ticketRegPrice * 40) / 100));
+
+    let result = 0;
+
+    if (age < 18) {
+        result = ticket20;
+    }
+    else if (age > 65) {
+        result = ticket40;
+    }
+    else {
+        result = ticketRegPrice;
+    }
 
     ticket.innerHTML = `
              <div class="d-flex justify-content-between">
-                <div>
-                    <h2>Nome passeggero</h2>
+                <div class="col-4">
+                    <div>
+                        <h5 class="text-uppercase">Nome passeggero</h5>
+                        <p>${name}</p>
+                    </div>
                 </div>
-                <div class="col-8 ">
-                    <p>Offerta</p>
+                <div class="col-8 d-flex justify-content-between ">
+                    <p class="offerta">Offerta</p>
+                    <p></p>
                     <p>Carrozza</p>
                     <p>Codice CP</p>
                     <p>Costo biglietto</p>
