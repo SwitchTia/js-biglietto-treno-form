9fabe8e91948d679ac60521d26ea975f1df32a3b
SwitchTia <oblasser.tatiana@gmail.com>
2025-11-06 18:48:13 +0100

    Fix: HTML

--- js/script.js
+++ js/script.js
@@ -14,12 +14,29 @@ const kmInput = document.getElementById("km");
 const ageInput = document.getElementById("age");
 
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  
-  const name = nameInput.value;
-  const km = kmInput.value;
-  const age = ageInput.value;
+const ticket = document.querySelector(".ticket");
+//const ticketHeader = document.querySelector(".ticket-header");
+//const ticketBody = document.querySelector(".ticket-body");
 
-  //da completare
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = nameInput.value;
+    const km = kmInput.value;
+    const age = ageInput.value;
+
+    ticket.innerHTML = `
+             <div class="d-flex justify-content-between">
+                <div>
+                    <h2>Nome passeggero</h2>
+                </div>
+                <div class="col-8 ">
+                    <p>Offerta</p>
+                    <p>Carrozza</p>
+                    <p>Codice CP</p>
+                    <p>Costo biglietto</p>
+                </div>
+            </div>
+            `;
 });
